3901bb3afa3d369f7e434638c2691b6d72cdd74c
Paul Scott <8pscott@gmail.com>
2021-08-03 00:16:15 -0400

    css cleanup

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import { KartStats } from '../KartStats/KartStats';
+import React from 'react';
+import { PartContext } from '../../providers/PartProvider';
+import { sumOfStats } from '../../util/Common';
 import Optimizer from '../Optimizer/Optimizer';
+import ProgressBar from '../ProgressBar/ProgressBar';
 import './Sidebar.scss';
 
-export default class Sidebar extends Component {
-  render() {
-    return (
-      <div className="Sidebar">
-        <KartStats></KartStats>
-        <Optimizer></Optimizer>
+const Sidebar: React.FC = () => {
+  const partData = React.useContext(PartContext);
+
+  const kartStats = sumOfStats([
+    partData.selectedDriver,
+    partData.selectedBody,
+    partData.selectedTire,
+    partData.selectedGlider,
+  ]);
+
+  return (
+    <div className="Sidebar">
+      <div className="KartStats">
+        <ProgressBar label="Speed" value={kartStats[0]}></ProgressBar>
+        <ProgressBar label="Acceleration" value={kartStats[1]}></ProgressBar>
+        <ProgressBar label="Weight" value={kartStats[2]}></ProgressBar>
+        <ProgressBar label="Handling" value={kartStats[3]}></ProgressBar>
+        <ProgressBar label="Traction" value={kartStats[4]}></ProgressBar>
       </div>
-    );
-  }
-}
+      <Optimizer></Optimizer>
+    </div>
+  );
+};
+
+export default Sidebar;
